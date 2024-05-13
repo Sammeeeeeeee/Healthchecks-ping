d425c0296c41d9052a016c40da0a04de50d4a3ba
Sammeeeeeeee <s.patcas18@gmail.com>
2024-05-13 22:28:42 +0100

    impliment better logging

--- curl.sh
+++ curl.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-echo "$(date) - Starting curl job:"
+echo "$(date) - Starting curl.sh:"
 
 if [ -z "$UUID" ]; then
     echo "ERROR: No UUID provided in UUID variable."
@@ -19,14 +19,11 @@ else
 fi 
 
 if ! crontab -l 2>/dev/null | grep -q "$CRON"; then
-    echo "ERROR: Invalid CRON expression or CRON not defined."
+    echo "ERROR: Invalid CRON expression or CRON not defined. See docs at https://github.com/Sammeeeeeeee/Healthchecks-ping"
     exit 1
 else
-    echo "CRON expression is OK. Next 3 scheduled runs:"
-    for i in {1..3}; do
-        echo "Run $i: $(date -d "$(date +'%Y-%m-%d %H:%M:%S')" --date="$(/usr/sbin/crontab -l | grep -E "$CRON" | /usr/bin/crontab -l | awk '{print $1, $2, $3, $4, $5}') +$((i * 60)) seconds" +'%Y-%m-%d %H:%M:%S')"
-    done
-fi
+    echo "CRON is OK"
+fi  
 
 echo "Building URL..."
 
@@ -34,7 +31,7 @@ URL="https://hc-ping.com/$UUID"
 
 echo "URL is OK"
 
-echo "Trying CURL.."
+echo "Trying to CURL Healthchecks..."
 
 if curl -s -o /dev/null -m "$TIMEOUT" --retry "$RETRY" "$URL"; then
     echo "CURL OK"
@@ -42,4 +39,4 @@ else
     echo "$(date) - CURL FAILED"
     exit 1
 fi
-echo "$(date) - curl job End"
+echo "$(date) - curl.sh END"
--- start.sh
+++ start.sh
@@ -2,32 +2,36 @@
 
 set -e
 
-echo "Starting start.sh:"
+echo "Initializing start.sh:"
 
-echo "Exporting UUID, TIMOUT and RETRY..."
+echo "Exporting UUID, TIMEOUT, and RETRY variables..."
 
 export UUID=$UUID
 export TIMEOUT=${TIMEOUT:-10}
 export RETRY=${RETRY:-5}
 
-echo "Timeout set to: $TIMEOUT seconds"
-echo "Retry count set to: $RETRY times"
+echo "Timeout set to: $TIMEOUT seconds."
+echo "Retry count set to: $RETRY times."
 
+echo "verifing curl.sh"
 if [ -x "/curl.sh" ]; then
-    echo "curl.sh script OK"
+    echo "curl.sh script verification successful."
+    echo "Adding curl.sh to cron jobs..."
     echo "$CRON /curl.sh" >> /var/spool/cron/crontabs/root
-    echo "CRON job set to: $CRON"
+    echo "CRON job successfully set with frequency: $CRON."
 else
-    echo "CRITICAL: curl.sh script not found."
+    echo "CRITICAL: curl.sh script not found. Please repull the image, or submit an issue at https://github.com/Sammeeeeeeee/Healthchecks-ping/issues"
 fi
 
 if [ "$WEB" = "true" ]; then
-    echo "Starting nginx..."
+    echo "Starting nginx server..."
     nginx
-    echo "Started nginx"
+    echo "Nginx server successfully started."
+else 
+    echo "No web server set. Start web UI with -e WEB="true"."
 fi
 
-echo "Web OK."
-echo "Running cron in foreground. Expect no more output from start.sh. Start.sh OK"
+echo "Web server status: OK."
+echo "Running cron jobs in foreground. No further output expected from start.sh. Initialization complete."
 crond -l 2 -f
-echo "If you are seeing this, CRON failed."
+echo "CRON execution failed. Check logs for details, or open an issue at https://github.com/Sammeeeeeeee/Healthchecks-ping/issues"
